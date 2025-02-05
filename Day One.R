95edc039206338bb5558ef7006e001f9be075c8a
Xivumbiwa Maswanganyi <MSWXIV001@MYUCT.AC.ZA>
2025-02-05 10:23:32 +0200

    Initial commit

---
+++ Day One.R
@@ -0,0 +1,23 @@
+xcode-select -- install
+
+git --version
+
+install.packages("usethis")
+
+if (!requireNamespace("usethis", quietly= TRUE)){
+  install.packages("usethis")
+}
+gert::git_config_global()
+
+username<- "Xivumbiwa Maswanganyi"
+email <- "MSWXIV001@MYUCT.AC.ZA"
+
+stopifnot(!is.null(username))
+stopifnot(!is.null(email))
+
+gert::git_config_global_set("user.name",username)
+gert::git_config_global_set("user.email",email)
+
+
+usethis:: use_git()
+11
